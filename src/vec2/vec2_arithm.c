599f1900885710d7e5256f6c627dc8c9df4b983c
Alexis Hachemi <alexishachemi@gmail.com>
2024-08-25 02:20:00 +0200

    Add vec2 div safeguards

--- src/vec2/vec2_arithm.c
+++ src/vec2/vec2_arithm.c
@@ -17,5 +17,11 @@ vec2_t vec2_mul(vec2_t a, vec2_t b)
 
 vec2_t vec2_div(vec2_t a, vec2_t b)
 {
-    return (vec2_t){a.x / b.x, a.y / b.y};
+    vec2_t c = {0, 0};
+
+    if (b.x != 0)
+        c.x = a.x / b.x;
+    if (b.y != 0)
+        c.y = a.y / b.y;
+    return c;
 }
--- src/vec2/vec2_int_arithm.c
+++ src/vec2/vec2_int_arithm.c
@@ -17,5 +17,11 @@ vec2_t vec2_mul_i(vec2_t a, int n)
 
 vec2_t vec2_div_i(vec2_t a, int n)
 {
-    return (vec2_t){a.x / n, a.y / n};
+    vec2_t c = {0, 0};
+
+    if (n != 0) {
+        c.x = a.x / n;
+        c.y = a.y / n;
+    }
+    return c;
 }
